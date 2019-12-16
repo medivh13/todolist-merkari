9d517f4ea59d53e5e42cacc99ab3cca2bd8d5fd4
jody almaida putra <jody.almaida@gmail.com>
2019-12-16 14:06:01 +0700

    initial commit

---
+++ app/Events/TaskCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TaskCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $task;
+    public function __construct($task)
+    {
+        //
+        $this->task = $task;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('newTask');
+    }
+
+    public function broadcastAs(){
+        return 'task-created';
+    }
+}
